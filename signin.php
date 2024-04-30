8807aad7ae24f46c437e2aeab0b286365a143809
OlegPazny <petr.jakimovets@gmail.com>
2024-04-30 13:15:44 +0300

    стиль авторизации

--- signin.php
+++ signin.php
@@ -8,15 +8,15 @@ session_start();
 
 // Обновляем время жизни сессии
 if (isset($_SESSION['last_activity']) && time() - $_SESSION['last_activity'] > $session_lifetime) {
-    // Если прошло более $session_lifetime секунд с момента последней активности, уничтожаем сессию
-    session_unset();
-    session_destroy();
-    session_start(); // Начинаем новую сессию
-} 
+  // Если прошло более $session_lifetime секунд с момента последней активности, уничтожаем сессию
+  session_unset();
+  session_destroy();
+  session_start(); // Начинаем новую сессию
+}
 
 $_SESSION['last_activity'] = time(); // Обновляем время последней активности
-  require_once "assets/api/get_bg.php";
-  require_once "assets/api/quiz_name_script.php";
+require_once "assets/api/get_bg.php";
+require_once "assets/api/quiz_name_script.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,90 +36,131 @@ $_SESSION['last_activity'] = time(); // Обновляем время после
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
   <link href="assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="assets/demo/demo.css" rel="stylesheet" />
-  <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
 </head>
 <style>
-  .message{
-        color:#951212;
-        font-size:15px;
-    }
-    .none{
-        display: none;
-    }
-    .error{
-        border-bottom:1px solid red;
-    }
+  body{
+    background-image: url("assets/img/Картинки/bg_1.svg");
+    background-size: cover;
+  }
+  .container{
+    height:100vh;
+    width:25%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .form-container{
+    width:100%;
+  }
+  .input-container{
+    display: flex;
+    flex-direction: column;
+    width:100%;
+    background-color: white;
+    border:1px solid #4082be;
+    border-radius:25px;
+    padding:45px 35px;
+    padding-top:30px;
+  }
+  .head{
+    width:100%;
+    border:1px solid #4082be;
+    background-color: #1163ae;
+    width:fit-content;
+    height:fit-content;
+    border-radius:25px;
+    padding:25px 35px;
+    display: flex;
+    flex-direction:column;
+    align-items: center;
+    margin-bottom:20px;
+  }
+  .head h3{
+    color:white;
+    font-size:45px;
+    font-weight: bold;
+    text-align: center;
+  }
+  .head h4{
+    font-size:30px;
+    font-weight: 500;
+    color:white;
+  }
+  .form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  input[type="text"], input[type="password"]{
+    height:60px;
+    border:none;
+    border-bottom: 1px solid #dcdcdc;
+  }
+  input[type="text"]::placeholder, input[type="password"]::placeholder{
+    font-weight: bold;
+    color:#7f7f7f;
+  }
+  .login-btn{
+    border:none;
+    background-color: #78b64e;
+    color:white;
+    border-radius: 5px;
+    font-size: 24px;
+    padding:10px 25px;
+    margin-top:10px;
+    align-self: center;
+  }
+  .message {
+    color: #951212;
+    font-size: 15px;
+  }
+
+  .none {
+    display: none;
+  }
+
+  .error {
+    border-bottom: 1px solid red;
+  }
 </style>
-<body class="login-page sidebar-collapse">
-  <div class="page-header"
-    style="background-image: url('data:image/jpeg;base64,<?php echo($bg);?>'); background-size: cover; background-position: top center;">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4 col-md-6 ml-auto mr-auto">
-          <div class="card card-login">
-
-
-
-            <form class="form">
-              <div class="card-header card-header-info text-center">
-                <h3><?php echo($quiz_name); ?></h3>
-                <h4 class="card-title">Авторизация</h4>
-              </div>
-              <div class="card-body">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="material-icons">supervised_user_circle</i>
-                    </span>
-                  </div>
-
-
-                  <input type="text" class="form-control" id="text" aria-describedby="emailHelp" name="login" placeholder="Логин...">
-
-
-                </div>
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="material-icons">lock_outline</i>
-                    </span>
-                  </div>
-
-
-                  <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Пароль...">
-
-                  
-                </div>
-              </div>
-              <div class="footer text-center">
-                <input type="submit" class=" login-btn btn btn-info btn-link btn-wd btn-lg" value="Войти" />
-                <script src="assets/js/auth.js"></script>
-              </div>
-            </form>
-
-
-
-          </div>
+<!-- style="background-image: url('data:image/jpeg;base64,<?php echo ($bg); ?>'); background-size: cover; background-position: top center;" -->
+<body>
+  <div class="container">
+    <div class="head">
+      <h3><?php echo ($quiz_name); ?></h3>
+      <h4>Авторизация</h4>
+    </div>
+    <div class="form-container">
+      <form class="form">
+        <div class="input-container">
+          <input type="text" id="text" aria-describedby="emailHelp" name="login" placeholder="Логин">
+          <input type="password" name="password" id="exampleInputPassword1" placeholder="Пароль">
         </div>
-      </div>
+        <input type="submit" class="login-btn" value="Войти" />
+      </form>
     </div>
   </div>
-  <!--   Core JS Files   -->
-  <script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-  <script src="assets/js/plugins/moment.min.js"></script>
-  <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-  <script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
-  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
-  <!--	Plugin for Sharrre btn -->
-  <script src="assets/js/plugins/jquery.sharrre.js" type="text/javascript"></script>
-  <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-  <script src="assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script>
+
 </body>
+<script src="assets/js/auth.js"></script>
+<!--   Core JS Files   -->
+<script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+<script src="assets/js/plugins/moment.min.js"></script>
+<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+<script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--	Plugin for Sharrre btn -->
+<script src="assets/js/plugins/jquery.sharrre.js" type="text/javascript"></script>
+<!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+<script src="assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script>
 
 </html>
